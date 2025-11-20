a4a458b0b12fac44ebe3137e473ae56eb8324947
Xiang <xiangxzou@global.tencent.com>
2025-11-20 11:02:52 +0100

    fix error

--- model/vgg.py
+++ model/vgg.py
@@ -30,9 +30,11 @@ class VGG19(torch.nn.Module):
                 param.requires_grad = False
         
         self.pool = nn.AdaptiveAvgPool2d(output_size=1)
-                
-        self.mean = torch.tensor([0.485, 0.456, 0.406]).view(1,-1, 1, 1).cuda() * 2 - 1
-        self.std = torch.tensor([0.229, 0.224, 0.225]).view(1,-1, 1, 1).cuda() * 2
+
+        mean = torch.tensor([0.485, 0.456, 0.406]).view(1, -1, 1, 1) * 2 - 1
+        std = torch.tensor([0.229, 0.224, 0.225]).view(1, -1, 1, 1) * 2
+        self.register_buffer("mean", mean)
+        self.register_buffer("std", std)
 
     def forward(self, X): # relui_1
         X = (X-self.mean)/self.std
